e313a5e04b7235e1e761e7c0b783b70306fda23d
DinoSnake888 <jamie.manalang@ht-la.org>
2026-03-23 10:22:41 -0700

    Initial commit

---
+++ Codes/BagProject/src/Bag.java
@@ -0,0 +1,27 @@
+public abstract class Bag{
+    int bagStorage = 10; // at most 10 items cause why not
+    double weight;
+    double age;
+
+    public Bag(){
+        int itemCapacity;
+    }
+    //Methods for actions to do with the bag
+    public void addItem(){
+        //adds an item to the bag
+    }
+    public void removeItem(){
+        //removes an item from the bag
+    }
+    public void incinerate(){
+        //destroys everything in the bag
+    }
+
+}
+
+/*
+    I want to try and make it so that you destroy everything in the bag with incinerate but
+    if it's too difficult I will just remove it.
+    I also want to add a way to make it so that a bag becomes a mary poppins pag so that teh
+    10 item limit is now void and any amount if items can be added to the bag
+*/
