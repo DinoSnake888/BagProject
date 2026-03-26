8c5a9edadf3f27b1a656e564415fb109defe98e1
DinoSnake888 <jamie.manalang@ht-la.org>
2026-03-25 23:47:12 -0700

    add an Items class

---
+++ Codes/BagProject/src/Items.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Items {
+    HashMap<Integer, String> itemThing = new HashMap<>();
+}
+
+class Sword extends Items{
+
+}
+
+class Pear extends Items{
+
+}
+
+class Potion extends Items{
+    
+}
